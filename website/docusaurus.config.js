3f522cff0a206c6bb68f24ee0ed961a67c129382
Sebastien Kister <sebastien@arianesline.com>
2026-01-09 12:33:48 -0600

    Rename 'Manual' to 'User Manual' and update link to 'docs/Starter'

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -101,8 +101,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Manual',
-                to: '/',
+                label: 'User Manual',
+                to: 'docs/Starter',
               },
             ],
           },
